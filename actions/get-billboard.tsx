61a436e2de4e4b952d8b16e29117f99d32033c54
George Lungu <george.lungu1896@gmail.com>
2023-10-23 15:27:55 +0300

    first commit was wrong

--- actions/get-billboard.tsx
+++ actions/get-billboard.tsx
@@ -3,7 +3,7 @@ import { Billboard } from "@/types";
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
 const getBillboard = async (id: string): Promise<Billboard> => {
-  const res = await fetch(`${URL}/billboards`);
+  const res = await fetch(`${URL}`);
 
   return res.json();
 };
